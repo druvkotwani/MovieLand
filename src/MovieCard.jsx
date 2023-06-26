21caa9542a47f3cedb351a20a6bda7bdf560de4e
Druv <druvkotwani12@gmail.com>
2023-06-26 08:46:39 +0530

    "some changes"

--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -6,19 +6,21 @@ const MovieCard = ({ movie }) => {
       <div>
         <p>{movie.Year}</p>
       </div>
+
       <div>
         <img
           src={
-            movie.Poster != "N/A"
+            movie.Poster !== "N/A"
               ? movie.Poster
               : "https://via.placeholder.com/400"
           }
-          alt={movie}
+          alt={movie.Title}
         />
       </div>
+
       <div>
-        <span>{movie.type}</span>
-        <h3>{movie.title}</h3>
+        <span>{movie.Type}</span>
+        <h3>{movie.Title}</h3>
       </div>
     </div>
   );
